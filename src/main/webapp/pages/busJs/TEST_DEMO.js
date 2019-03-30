38ce2c9c284b92c0d938f347fec8c989a73f2e1e
wzl <1968453499@qq.com>
2019-03-30 13:39:44 +0800

    演示js文件

--- src/main/webapp/pages/busJs/TEST_DEMO.js
+++ src/main/webapp/pages/busJs/TEST_DEMO.js
@@ -1,11 +1,11 @@
 buttonJson =[
-              {name:'查询',fun:'queryTable(this)',buttonToken:'query'},
-              {name:'新增',fun:'setUpAdd(this),tog(this)',buttonToken:'add'},
-              {name:'修改',fun:'updateRow(this)',buttonToken:'update'},           
-              {name:'删除',fun:'deleteRowCheck(this)',buttonToken:'delete'},
-              {name:'导入',fun:'upload(this)',buttonToken:'upload'},
-              {name:'洽谈业务',fun:'jump(this)',buttonToken:'third'}
-              ];
+    {name: '查询', fun: 'queryTable(this)', buttonToken: 'query'},
+    {name: '新增', fun: 'setUpAdd(this),tog(this)', buttonToken: 'add'},
+    {name: '修改', fun: 'updateRow(this)', buttonToken: 'update'},
+    {name: '删除', fun: 'deleteRowCheck(this)', buttonToken: 'delete'},
+    {name: '导入', fun: 'upload(this)', buttonToken: 'upload'},
+    {name: '洽谈业务', fun: 'jump(this)', buttonToken: 'third'}
+];
 //导入初始化 必须 否则页面功能有问题
 $(function(){
 	var fileInput=new FileInput();
@@ -13,7 +13,7 @@ $(function(){
 });
 //重写参照
 function ref_write_json(rejsonArray){
-	//参照 选择json 做单独的处理 
+    //参照 选择json 做单独的处理
 	console.log(JSON.stringify(rejsonArray));
 	return false; //return true 继续重写页面字段  false 不做页面回写
 }
@@ -32,7 +32,7 @@ function deleteRowCheck(){
 	}else{
 		delRows(t);
 	}
-	
+
 }
 
 //双击事件  列表模版双击事件跳转其他操作
@@ -40,11 +40,12 @@ function dblClickFunction(row,tr){
 	var json =JSON.parse(JSON.stringify(row));
 	console.log(json);
 }
-//业务跳转 
+
+//业务跳转
 
 function jump(t){
 	window.location.href=context+"/pages/childTableModify_persion.jsp?pageCode=MD_PERSONNEL&token="+token;
-	
+
 }
 function ref_end(){
 	console.log("参选回调用");
@@ -76,11 +77,11 @@ function initDetailTable(parentId){
 //初始化子表列表
 function initDetailTableList(){
 	var tableHtml = '<div class="col-md-12" style="margin-top:10px; margin-bottom:10px; padding:0;">'+
-						'<button type="button" class="btn btn-primary" id="insert" onclick="insertDetail();">新增</button>&nbsp;'+
-						'<button type="button" class="btn btn-warning" id="update" onclick="updateDetail();">修改</button>&nbsp;'+
-				   		'<button type="button" class="btn btn-danger" id="delete" onclick="deleteDetail();">删除</button>'+
-					'</div>'+
-					'<table id="detailTable" data-row-style="rowStyle"></table>';
+        '<button type="button" class="btn btn-primary" id="insert" onclick="insertDetail();">新增</button>&nbsp;' +
+        '<button type="button" class="btn btn-warning" id="update" onclick="updateDetail();">修改</button>&nbsp;' +
+        '<button type="button" class="btn btn-danger" id="delete" onclick="deleteDetail();">删除</button>' +
+        '</div>' +
+        '<table id="detailTable" data-row-style="rowStyle"></table>';
 	$('#insPage').append(tableHtml);
 }
 
@@ -89,13 +90,13 @@ function initDetailDiv(){
 	var fromHtml = '<div class="panel-body bv-form" id="detailDiv" novalidate="novalidate" style="display:none;"></div>';
 	$('#insPage').after(fromHtml);
 	var buttonHtml = '<div id="detailButton" style="display:none;">'+
-					   '<button type="button" class="btn btn-success" onclick="detailSave(this)">'+
-							'<span class="glyphicon glyphicon" aria-hidden="true"></span>保存'+
-					   '</button>&nbsp;'+
-					   '<button type="button" class="btn btn-inverse" onclick="detailBack(this)">'+
-							'<span class="glyphicon glyphicon" aria-hidden="true"></span>返回'+
-					   '</button>'+
-					'</div>';
+        '<button type="button" class="btn btn-success" onclick="detailSave(this)">' +
+        '<span class="glyphicon glyphicon" aria-hidden="true"></span>保存' +
+        '</button>&nbsp;' +
+        '<button type="button" class="btn btn-inverse" onclick="detailBack(this)">' +
+        '<span class="glyphicon glyphicon" aria-hidden="true"></span>返回' +
+        '</button>' +
+        '</div>';
 	$('#detailDiv').before(buttonHtml);
 }
 //删除验证
@@ -173,14 +174,14 @@ function detailBack(t){
 //子表保存
 function detailSave(){
 	$('#detailDiv').data("bootstrapValidator").validate();
-    if(!$('#detailDiv').data('bootstrapValidator').isValid()){ 
-    	setParntHeigth($("body").height());
-        return ; 
+    if (!$('#detailDiv').data('bootstrapValidator').isValid()) {
+        setParntHeigth($("body").height());
+        return;
     }
     divDisplay();
-    bulidValid($('#detailDiv'),validJsonDetail);
+    bulidValid($('#detailDiv'), validJsonDetail);
 	var json = JSON.parse(getJson($('#detailDiv')));
-	if(json['ID'] == ""){	
+    if (json['ID'] == "") {
 		var tableData = $('#detailTable').bootstrapTable("getData");
 		if(tableData != null && tableData.length >= 0){
 			detailTableIndex = tableData.length + 1;
@@ -192,7 +193,7 @@ function detailSave(){
 		$('.pagination-info').html("共"+detailTableIndex+"条记录");
 	}else{
 		json = jsonHandle(json);
-		$('#detailTable').bootstrapTable('updateByUniqueId', {id: json['ID'], row: json}); 
+        $('#detailTable').bootstrapTable('updateByUniqueId', {id: json['ID'], row: json});
 	}
 }
 
@@ -205,7 +206,7 @@ function clearForm(){
 //子表div显示
 function detailTableShow(){
 	$('#detailDiv').css('display','block');
-	$('#detailButton').css('display','block');	
+    $('#detailButton').css('display', 'block');
 }
 //主表div隐藏
 function mainTableHide(){

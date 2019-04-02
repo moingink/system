c567ae94303303a5172d1bd95ed547aadf3702ac
changjr <changjr@LAPTOP-AHFAVNGB>
2019-04-02 15:10:07 +0800

    新增 主数据关系映射 按钮类

--- src/main/java/com/yonyou/web/ButtonBase.java
+++ src/main/java/com/yonyou/web/ButtonBase.java
@@ -79,155 +79,166 @@ import com.yonyou.business.button.util.ButForUpdateStatus;
 import com.yonyou.business.button.util.IncomeExpor;
 import com.yonyou.business.button.util.system.ButForInsertTestDemo;
 import com.yonyou.business.button.util.system.ButForUpdateTestDemo;
-
+import com.yonyou.business.orangefinancial.button.BtnForDeleteM;
+import com.yonyou.business.orangefinancial.button.BtnForInsertM;
+import com.yonyou.business.orangefinancial.button.BtnForUpdateM;
 
 @RestController
 @RequestMapping(value = "/buttonBase")
 public class ButtonBase extends ButtonController {
 
-	
-	private Map<String,ButtonAbs> buttonMap =new HashMap<String,ButtonAbs>();
+	private Map<String, ButtonAbs> buttonMap = new HashMap<String, ButtonAbs>();
 	{
-		/*************************公共的*****************/
-		buttonMap.put("add", 	new ButForInsert());//新增
-		buttonMap.put("doem", 	new ButForInsert());//新增
-		buttonMap.put("update", new ButForUpdate());//修改 
-		buttonMap.put("query", 	new ButForSelect());//查询
-		buttonMap.put("delete", new ButForDeleteForDb());//删除 //应demo需要 暂换成物理删除  ；ButForDelete 为逻辑删除  --moing_Ink
-		buttonMap.put("physicalDelete", new ButForPhysicalDelete());//物理删除
 		
-		
-		/*************************demo*****************/
-		buttonMap.put("button_check", new DemoButForCheck());//复核
-		buttonMap.put("button_checkback", new DemoButForCheckBack());//撤销复核
-		buttonMap.put("button_import", new DemoButForImport());//Excel导入
-		buttonMap.put("updateByDemo", new DemoButForUpdate());//修改
-		
-		/*************************带缓存的数据的增删改*****************/
-		buttonMap.put("insertWithCache", new ButForInsertWithCache());//新增
-		buttonMap.put("updateWithCache", new ButForUpdateWithCache());//修改
-		buttonMap.put("deleteWithCache", new ButForDeleteWithCache());//删除
-		
-		/*************************组织管理的增删改*****************/
-		buttonMap.put("addCompany", new PartyButForInsertCompany());//新增
-		buttonMap.put("addDepartment", new PartyButForInsertDepartment());//新增
-		buttonMap.put("deleteParty", new PartyButForDelete());//delete
-		buttonMap.put("updateParty", new PartyButForUpdate());//update
-		
-		/*************************按钮管理*****************/
-		buttonMap.put("but_rel_menu_bind", 	 new SystemButtonButForBind());//菜单按钮关联绑定
-		buttonMap.put("but_rel_menu_unbind", new SystemButtonButForUnbind());//菜单按钮关联解绑
-		buttonMap.put("but_rel_menu_insert", new SystemButtonButForInsert());//菜单按钮关联新增
-		buttonMap.put("but_rel_menu_update", new SystemButtonButForUpdate());//菜单按钮关联修改
-		buttonMap.put("but_rel_menu_delete", new SystemButtonButForDelete());//菜单按钮关联删除
-		buttonMap.put("button_button_insert", new SystemButtonButForInsertButton());//按钮新增
-		buttonMap.put("button_button_update", new SystemButtonButForUpdateButton());//按钮修改
-		
+		/************************* 公共的 *****************/
+		buttonMap.put("add", new ButForInsert());// 新增
+		buttonMap.put("doem", new ButForInsert());// 新增
+		buttonMap.put("update", new ButForUpdate());// 修改
+		buttonMap.put("query", new ButForSelect());// 查询
+		buttonMap.put("delete", new ButForDeleteForDb());// 删除 //应demo需要 暂换成物理删除 ；ButForDelete 为逻辑删除 --moing_Ink
+		buttonMap.put("physicalDelete", new ButForPhysicalDelete());// 物理删除
 
-		/*************************角色管理的增删改*****************/
-		buttonMap.put("addRole", new RoleButForInsert());//新增
-		buttonMap.put("deleteRole", new RoleButForDelete());//删除角色
-		buttonMap.put("userSetting", new RoleButForUserSetting());//用户设置
-		//buttonMap.put("deletePartyRole", new PartyRoleButForDelete());//删除角色关系
-		buttonMap.put("roleMutexSetting", new RoleButForRoleMutexSetting());//用户设置
-		buttonMap.put("roleMutexDelete", new RoleButForRoleMutexDelete());//互斥角色删除
-		buttonMap.put("role_grant_to_button", new RoleButForGrantToButton());//角色按钮授权
-		buttonMap.put("role_query_to_button", new RoleButForQueryToButton());//角色按钮授权查询
-		buttonMap.put("modify_myself_authority", new RoleButForModifyAuthorizeToButton());//修改本人
-		buttonMap.put("modify_company_authority", new RoleButForModifyAuthorizeToButton());//修改本机构
-		buttonMap.put("modify_all_authority", new RoleButForModifyAuthorizeToButton());//修改所有
-		
-		buttonMap.put("nextAuth2", new RoleButtonAuthNext());//二级页面授权按钮
-		
-		/*************************用户管理的增删改*****************/
-		buttonMap.put("addUser", new UserButForInsert());//新增
-		buttonMap.put("updateUser", new UserButForUpdate());//修改
-		buttonMap.put("deleteUser", new UserButForDelete());//删除
-		buttonMap.put("activationUser", new UserButForActivation());//激活
-		buttonMap.put("lockUser", new UserButForLock());//锁定
-		buttonMap.put("resetPassword", new UserButForResetPassword());//锁定
-		buttonMap.put("roleSetting", new UserButForRoleSetting());//角色设置
-		buttonMap.put("passwordStrategy", new UserButForPasswordStrategy());//角色设置
-		/*************************利用存储过程转化并插入子表*****************/
+		/************************* demo *****************/
+		buttonMap.put("button_check", new DemoButForCheck());// 复核
+		buttonMap.put("button_checkback", new DemoButForCheckBack());// 撤销复核
+		buttonMap.put("button_import", new DemoButForImport());// Excel导入
+		buttonMap.put("updateByDemo", new DemoButForUpdate());// 修改
+
+		/************************* 带缓存的数据的增删改 *****************/
+		buttonMap.put("insertWithCache", new ButForInsertWithCache());// 新增
+		buttonMap.put("updateWithCache", new ButForUpdateWithCache());// 修改
+		buttonMap.put("deleteWithCache", new ButForDeleteWithCache());// 删除
+
+		/************************* 组织管理的增删改 *****************/
+		buttonMap.put("addCompany", new PartyButForInsertCompany());// 新增
+		buttonMap.put("addDepartment", new PartyButForInsertDepartment());// 新增
+		buttonMap.put("deleteParty", new PartyButForDelete());// delete
+		buttonMap.put("updateParty", new PartyButForUpdate());// update
+
+		/************************* 按钮管理 *****************/
+		buttonMap.put("but_rel_menu_bind", new SystemButtonButForBind());// 菜单按钮关联绑定
+		buttonMap.put("but_rel_menu_unbind", new SystemButtonButForUnbind());// 菜单按钮关联解绑
+		buttonMap.put("but_rel_menu_insert", new SystemButtonButForInsert());// 菜单按钮关联新增
+		buttonMap.put("but_rel_menu_update", new SystemButtonButForUpdate());// 菜单按钮关联修改
+		buttonMap.put("but_rel_menu_delete", new SystemButtonButForDelete());// 菜单按钮关联删除
+		buttonMap.put("button_button_insert", new SystemButtonButForInsertButton());// 按钮新增
+		buttonMap.put("button_button_update", new SystemButtonButForUpdateButton());// 按钮修改
+
+		/************************* 角色管理的增删改 *****************/
+		buttonMap.put("addRole", new RoleButForInsert());// 新增
+		buttonMap.put("deleteRole", new RoleButForDelete());// 删除角色
+		buttonMap.put("userSetting", new RoleButForUserSetting());// 用户设置
+		// buttonMap.put("deletePartyRole", new PartyRoleButForDelete());//删除角色关系
+		buttonMap.put("roleMutexSetting", new RoleButForRoleMutexSetting());// 用户设置
+		buttonMap.put("roleMutexDelete", new RoleButForRoleMutexDelete());// 互斥角色删除
+		buttonMap.put("role_grant_to_button", new RoleButForGrantToButton());// 角色按钮授权
+		buttonMap.put("role_query_to_button", new RoleButForQueryToButton());// 角色按钮授权查询
+		buttonMap.put("modify_myself_authority", new RoleButForModifyAuthorizeToButton());// 修改本人
+		buttonMap.put("modify_company_authority", new RoleButForModifyAuthorizeToButton());// 修改本机构
+		buttonMap.put("modify_all_authority", new RoleButForModifyAuthorizeToButton());// 修改所有
+
+		buttonMap.put("nextAuth2", new RoleButtonAuthNext());// 二级页面授权按钮
+
+		/************************* 用户管理的增删改 *****************/
+		buttonMap.put("addUser", new UserButForInsert());// 新增
+		buttonMap.put("updateUser", new UserButForUpdate());// 修改
+		buttonMap.put("deleteUser", new UserButForDelete());// 删除
+		buttonMap.put("activationUser", new UserButForActivation());// 激活
+		buttonMap.put("lockUser", new UserButForLock());// 锁定
+		buttonMap.put("resetPassword", new UserButForResetPassword());// 锁定
+		buttonMap.put("roleSetting", new UserButForRoleSetting());// 角色设置
+		buttonMap.put("passwordStrategy", new UserButForPasswordStrategy());// 角色设置
+		/************************* 利用存储过程转化并插入子表 *****************/
 		buttonMap.put("procedure", new ButForProcedure());
-		
-		/*************************物理新增、修改元数据字段*****************/
+
+		/************************* 物理新增、修改元数据字段 *****************/
 		buttonMap.put("metadataPhyIns", new ButForMetadataPhyIns());
 		buttonMap.put("metadataPhyUpd", new ButForMetadataPhyUpd());
-		
-		/*************************图片新增，修改（轮播图）*****************************/
-		buttonMap.put("addPic", new ButForAddPic());//新增
-		
-		/*************************主题新增，修改（轮播图）*****************************/
-		buttonMap.put("addThe", new ButForAddTheme());//新增
-		
-		/*************************角色主题新增（轮播图）*****************************/
-		buttonMap.put("addRoleTheme", new ButForAddRoleTheme());//新增
-		
-		/*************************自定义菜单*****************************/
-		buttonMap.put("addM", new ButForAddCuMenu());//新增
-		buttonMap.put("resetM", new ButForResetCuMenu());//重置
-		buttonMap.put("deleteM", new ButForDeleteCuMenu());//删除
-		
-		/*************************任务按钮*****************************/
-		buttonMap.put("jobInsert", new ButForJobInsert()); //任务新增
-		buttonMap.put("jobDelete", new ButForJobDelete()); //任务修改
-		buttonMap.put("jobStateUp", new ButForJobStateUp()); //任务启动
-		buttonMap.put("jobStop", new ButForJobStop()); //任务关闭
-		
-		/******************************公司 管理*****************************************/
+
+		/************************* 图片新增，修改（轮播图） *****************************/
+		buttonMap.put("addPic", new ButForAddPic());// 新增
+
+		/************************* 主题新增，修改（轮播图） *****************************/
+		buttonMap.put("addThe", new ButForAddTheme());// 新增
+
+		/************************* 角色主题新增（轮播图） *****************************/
+		buttonMap.put("addRoleTheme", new ButForAddRoleTheme());// 新增
+
+		/************************* 自定义菜单 *****************************/
+		buttonMap.put("addM", new ButForAddCuMenu());// 新增
+		buttonMap.put("resetM", new ButForResetCuMenu());// 重置
+		buttonMap.put("deleteM", new ButForDeleteCuMenu());// 删除
+
+		/************************* 任务按钮 *****************************/
+		buttonMap.put("jobInsert", new ButForJobInsert()); // 任务新增
+		buttonMap.put("jobDelete", new ButForJobDelete()); // 任务修改
+		buttonMap.put("jobStateUp", new ButForJobStateUp()); // 任务启动
+		buttonMap.put("jobStop", new ButForJobStop()); // 任务关闭
+
+		/****************************** 公司 管理 *****************************************/
 		buttonMap.put("addCom", new ButForInsertCompany());
-		/******************************部门 管理*****************************************/
+		/****************************** 部门 管理 *****************************************/
 		buttonMap.put("addDept", new ButForInsertDepartment());
-		
-		/******************************通知*******************************************/
+
+		/****************************** 通知 *******************************************/
 		buttonMap.put("send", new NotifyButForPlan());
-		buttonMap.put("addByNotity", 	new NotifyButForInsert());//新增
-		buttonMap.put("updateByNotity", new NotifyButForUpdate());//修改
-		
-		buttonMap.put("addByNoticeManage", 	new NoticeManageButForInsert());//新增
-		buttonMap.put("updateByNoticeManage", new NoticeManageButForUpdate());//修改
-		buttonMap.put("deleteByNoticeManage", new NoticeManageButForDelete());//修改
-		
-		/******************************审批*****************************************/
+		buttonMap.put("addByNotity", new NotifyButForInsert());// 新增
+		buttonMap.put("updateByNotity", new NotifyButForUpdate());// 修改
+
+		buttonMap.put("addByNoticeManage", new NoticeManageButForInsert());// 新增
+		buttonMap.put("updateByNoticeManage", new NoticeManageButForUpdate());// 修改
+		buttonMap.put("deleteByNoticeManage", new NoticeManageButForDelete());// 修改
+
+		/****************************** 审批 *****************************************/
 		buttonMap.put("audit", new ButForAudit());
-		/******************************修改表字段状态*****************************************/
+		/******************************
+		 * 修改表字段状态
+		 *****************************************/
 		buttonMap.put("updateStatus", new ButForUpdateStatus());
-		/******************************角色数据权限新增*****************************************/
+		/******************************
+		 * 角色数据权限新增
+		 *****************************************/
 		buttonMap.put("addForRoleDataAuth", new ButForInsertForRoleDataAuth());
-		
-		/******************************Excel导出*****************************************/
+
+		/******************************
+		 * Excel导出
+		 *****************************************/
 		buttonMap.put("zw_income_export", new IncomeExpor());
-		
-		/******************************业务流请求接口*****************************************/
+
+		/******************************
+		 * 业务流请求接口
+		 *****************************************/
 		buttonMap.put("busFlow", new ButForBusFlow());
-		
-		
-		
-		/******************************新增主子表*****************************************/
+
+		/****************************** 新增主子表 *****************************************/
 		buttonMap.put("addTestDemo", new ButForInsertTestDemo());
-		/******************************修改主子表*****************************************/
+		/****************************** 修改主子表 *****************************************/
 
 		buttonMap.put("updateTestDemo", new ButForUpdateTestDemo());
 
-		/******************************打印预览*****************************************/
+		/****************************** 打印预览 *****************************************/
 		buttonMap.put("printPreview", new ButForPrintPreview());
-		
+
+		// 主数据关系映射节点按钮组_BEGIN
+		/****************************** 新增主子表 *****************************************/
+		buttonMap.put("addMDEF", new BtnForInsertM());
+		/****************************** 修改主子表 *****************************************/
+		buttonMap.put("updateMDEF", new BtnForUpdateM());
+//		主数据关系 主子表删除
+		buttonMap.put("deleteMDEF", new BtnForDeleteM());
+		// 主数据关系映射节点按钮组_END
 	}
-	
+
 	@Override
 	protected Map<String, ButtonAbs> findButtonMap() {
 		// TODO 自动生成的方法存根
 		return buttonMap;
 	}
-	
-	
+
 	@Override
-	public void init(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public void init(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO 自动生成的方法存根
 		super.init(request, response);
 	}
 
-	
 }

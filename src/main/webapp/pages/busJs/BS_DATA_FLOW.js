d5dc07fd6af73ffe221f13b19650073db72340f2
jaker2018 <18910044120@163.com>
2019-04-10 09:44:54 +0800

    copy元数据

--- src/main/webapp/pages/busJs/BS_DATA_FLOW.js
+++ src/main/webapp/pages/busJs/BS_DATA_FLOW.js
@@ -5,5 +5,12 @@ buttonJson =[
              {name:'删除',fun:'delRows(this)',buttonToken:'deleteWithCache'},
  			];
 
-
+//双击事件  列表模版双击事件跳转其他操作
+function dblClickFunction(row,tr){
+	var json =JSON.parse(JSON.stringify(row));
+	console.log(json.ID);
+	//获取主表主键传递
+	//window.location.href=context+"/pages/test_demo.jsp?ParentPKValue="+json.ID+"&token="+token;
+	
+}
 

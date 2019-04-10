6a005ac30020af5f82cb9ce8291a0ff58bbd15aa
yancyary <443887451@qq.com>
2019-04-10 10:07:59 +0800

    1

--- src/main/webapp/pages/busJs/BS_DATA_ACQUISITION.js
+++
@@ -1,6 +0,0 @@
-buttonJson =[
-             {name:'查询',fun:'queryTable(this)',buttonToken:'query'},
-             {name:'新增',fun:'tog(this)',buttonToken:'insertWithCache'},
-             {name:'修改',fun:'updateRow(this)',buttonToken:'updateWithCache'},
-             {name:'删除',fun:'delRows(this)',buttonToken:'deleteWithCache'}
-			];
